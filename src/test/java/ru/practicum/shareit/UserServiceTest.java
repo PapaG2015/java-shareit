cf865c5e675ccde940cdb6dcb66233851646383b
Marat <karimovmar@yandex.ru>
2022-10-02 09:37:39 +0500

    ТЗ 15

--- src/test/java/ru/practicum/shareit/UserServiceTest.java
+++ src/test/java/ru/practicum/shareit/UserServiceTest.java
@@ -5,9 +5,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.user.UserService;
 import ru.practicum.shareit.user.dto.UserDto;
+
+import javax.transaction.Transactional;
 
 @Transactional
 @SpringBootTest(
@@ -20,11 +21,27 @@ public class UserServiceTest {
 
     @Test
     void testAddUser() {
-        UserDto userDto = new UserDto(1, "test", "test@yandex.ru");
-        userService.addUser(userDto);
+        UserDto userDto1 = new UserDto(1, "test1", "test1@yandex.ru");
+        userService.addUser(userDto1);
 
-        UserDto userDtoTest = userService.getUser(1);
+        UserDto userDto2 = new UserDto(2, "test2", "test2@yandex.ru");
+        userService.addUser(userDto2);
 
-        Assertions.assertEquals(userDtoTest.getName(), userDto.getName());
+        UserDto userDtoTest = userService.getUser(2);
+
+        Assertions.assertEquals(userDtoTest.getName(), "test2");
+    }
+
+    @Test
+    void testChangeUser() {
+        UserDto userDto3 = new UserDto(3, "test3", "test3@yandex.ru");
+        userService.addUser(userDto3);
+
+        UserDto userDto4 = new UserDto(4, "test4", "test4@yandex.ru");
+
+        userService.changeUser(3, userDto4);
+        UserDto userDtoTest = userService.getUser(3);
+
+        Assertions.assertEquals(userDtoTest.getName(), "test4");
     }
 }
