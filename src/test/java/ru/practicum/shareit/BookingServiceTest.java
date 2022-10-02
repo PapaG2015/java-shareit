5e3ca072d8492b56cc28320571c9a2aef578145e
Marat <karimovmar@yandex.ru>
2022-10-02 12:07:00 +0500

    ТЗ 15

--- src/test/java/ru/practicum/shareit/BookingServiceTest.java
+++ src/test/java/ru/practicum/shareit/BookingServiceTest.java
@@ -10,7 +10,6 @@ import ru.practicum.shareit.booking.dto.BookingDtoRequest;
 import ru.practicum.shareit.booking.dto.BookingDtoResponse;
 import ru.practicum.shareit.item.ItemService;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.UserService;
 import ru.practicum.shareit.user.dto.UserDto;
 
