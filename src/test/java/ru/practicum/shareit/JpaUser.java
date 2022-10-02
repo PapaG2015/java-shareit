2b17e4077cf4ae71339c6304bdf604735baf55a4
Marat <karimovmar@yandex.ru>
2022-10-02 14:26:25 +0500

    ТЗ 15

--- src/test/java/ru/practicum/shareit/JpaUser.java
+++ src/test/java/ru/practicum/shareit/JpaUser.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import ru.practicum.shareit.user.UserRepository;
 import ru.practicum.shareit.user.model.User;
 
