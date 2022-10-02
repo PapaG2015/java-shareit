81153514f3feafe233ac56cef6fe59e3d1decb3b
Marat <karimovmar@yandex.ru>
2022-10-02 09:53:08 +0500

    ТЗ 15

--- src/test/java/ru/practicum/shareit/UserServiceTest.java
+++ src/test/java/ru/practicum/shareit/UserServiceTest.java
@@ -9,6 +9,8 @@ import ru.practicum.shareit.user.UserService;
 import ru.practicum.shareit.user.dto.UserDto;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Transactional
 @SpringBootTest(
@@ -19,8 +21,8 @@ public class UserServiceTest {
 
     private final UserService userService;
 
-    @Test
-    void testAddUser() {
+    @Test //AddUser
+    void test1() {
         UserDto userDto1 = new UserDto(1, "test1", "test1@yandex.ru");
         userService.addUser(userDto1);
 
@@ -32,8 +34,8 @@ public class UserServiceTest {
         Assertions.assertEquals(userDtoTest.getName(), "test2");
     }
 
-    @Test
-    void testChangeUser() {
+    @Test //ChangeUser
+    void test2() {
         UserDto userDto3 = new UserDto(3, "test3", "test3@yandex.ru");
         userService.addUser(userDto3);
 
@@ -43,5 +45,21 @@ public class UserServiceTest {
         UserDto userDtoTest = userService.getUser(3);
 
         Assertions.assertEquals(userDtoTest.getName(), "test4");
+    }
+
+    @Test //GetUsers
+    void test3() {
+        UserDto userDto4 = new UserDto(4, "test5", "test5@yandex.ru");
+        userService.addUser(userDto4);
+
+        UserDto userDto5 = new UserDto(5, "test6", "test6@yandex.ru");
+        userService.addUser(userDto5);
+
+        List<UserDto> usersDto = userService.getUsers();
+        List<UserDto> usersDtoTest = new ArrayList<>();
+        usersDtoTest.add(userDto4);
+        usersDtoTest.add(userDto5);
+
+        Assertions.assertEquals(usersDto, usersDtoTest);
     }
 }
